13a54af5d85e4cc41a4180d83df75722823a2896
Andrew <life.around.freaks@gmail.com>
2021-04-09 15:08:23 +0300

    Video exception handling.

--- video.py
+++ video.py
@@ -36,40 +36,49 @@ def get_resolutions(url):
 
 def download(chat_id, url, resolution):
 
-    video_name, audio_name, out_name = (str(uuid4()) for _ in range(3))
-
-    yt = YouTube(validate_url(url))
-    video = yt.streams.filter(**VIDEO_SETTINGS).first().download(
-        filename=video_name)
-    video_stream = ffmpeg.input(video_name)
-    audio = yt.streams.filter(**AUDIO_SETTINGS).first().download(
-        filename=audio_name)
-    audio_stream = ffmpeg.input(audio_name)
-
     try:
-        ffmpeg.output(
-            audio_stream, video_stream, f'{out_name}.mp4'
-        ).run(quiet=True)
-    except ffmpeg.Error as e:
-        pass
 
-    video = ffmpeg.input(f'{video_name}.mp4')
-    audio = ffmpeg.input(f'{audio_name}.mp4')
-    out = ffmpeg.output(
-        video, audio, f'{out_name}.mp4', vcodec='copy',
-        acodec='aac', strict='experimental')
-    try:
-        out.run(quiet=True)
-    except ffmpeg.Error as e:
-        pass
+        video_name, audio_name, out_name = (str(uuid4()) for _ in range(3))
 
-    get_bot().send_video(
-        chat_id, open(f'{out_name}.mp4', 'rb'),
-        supports_streaming=True,
-    )
+        yt = YouTube(validate_url(url))
+        video = yt.streams.filter(**VIDEO_SETTINGS).first().download(
+            filename=video_name)
+        video_stream = ffmpeg.input(video_name)
+        audio = yt.streams.filter(**AUDIO_SETTINGS).first().download(
+            filename=audio_name)
+        audio_stream = ffmpeg.input(audio_name)
 
-    try:
-        for filename in (video_name, audio_name, out_name):
-            os.remove(f'{filename}.mp4')
-    except OSError:
-        pass
+        try:
+            ffmpeg.output(
+                audio_stream, video_stream, f'{out_name}.mp4'
+            ).run(quiet=True)
+        except ffmpeg.Error as e:
+            pass
+
+        video = ffmpeg.input(f'{video_name}.mp4')
+        audio = ffmpeg.input(f'{audio_name}.mp4')
+        out = ffmpeg.output(
+            video, audio, f'{out_name}.mp4', vcodec='copy',
+            acodec='aac', strict='experimental')
+        try:
+            out.run(quiet=True)
+        except ffmpeg.Error as e:
+            pass
+
+        get_bot().send_video(
+            chat_id, open(f'{out_name}.mp4', 'rb'),
+            supports_streaming=True,
+        )
+
+    except Exception as e:
+
+        get_bot().send_message(
+            chat_id, 'Sorry, something was wrong with your video.')
+
+    finally:
+
+        try:
+            for filename in (video_name, audio_name, out_name):
+                os.remove(f'{filename}.mp4')
+        except OSError:
+            pass
