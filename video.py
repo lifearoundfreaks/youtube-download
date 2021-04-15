6351f34c19f38d93859376b0d9d60b040940b1f0
Andrew <life.around.freaks@gmail.com>
2021-04-15 17:41:04 +0300

    Download code refactoring in an attempt to solve memory issues.

--- video.py
+++ video.py
@@ -5,24 +5,41 @@ from uuid import uuid4
 from utils import get_bot
 
 
+def run(command):
+
+    subprocess.run(command, shell=True, stdout=subprocess.DEVNULL)
+
+
+def download_stream(url, time_from, time_to, name):
+
+    run(f'ffmpeg -ss {time_from} -to {time_to} -i "{url}" "{name}.mp4"')
+
+
+def combine(video, audio, out):
+
+    run(f'ffmpeg -i {video}.mp4 -i {audio}.mp4 -c:v copy -c:a aac {out}.mp4')
+
+
+def silently_delete(*filenames):
+
+    for name in filenames:
+        try:
+            os.remove(f'{name}.mp4')
+        except OSError:
+            pass
+
+
 def download(chat_id, v_url, a_url, time_from, time_to):
 
     try:
+        video_name, audio_name, out_name = (str(uuid4()) for _ in range(3))
 
-        temp_name = str(uuid4())
-
-        command = (
-            f'ffmpeg -ss {time_from} -to {time_to} -i "{v_url}" '
-            f'-ss {time_from} -to {time_to} -i "{a_url}" '
-            "-acodec aac -b:a 192k -avoid_negative_ts make_zero "
-            f'-map 0:v:0 -map 1:a:0 "{temp_name}.mp4"'
-        )
-
-        subprocess.run(command, shell=True, stdout=subprocess.DEVNULL)
+        download_stream(v_url, time_from, time_to, video_name)
+        download_stream(a_url, time_from, time_to, audio_name)
+        combine(video_name, audio_name, out_name)
 
         get_bot().send_video(
-            chat_id, open(f'{temp_name}.mp4', 'rb'),
-            supports_streaming=True,
+            chat_id, open(f'{out_name}.mp4', 'rb'), supports_streaming=True,
         )
 
     except Exception as e:
@@ -33,7 +50,4 @@ def download(chat_id, v_url, a_url, time_from, time_to):
 
     finally:
 
-        try:
-            os.remove(f'{temp_name}.mp4')
-        except OSError:
-            pass
+        silently_delete(video_name, audio_name, out_name)
