523c5e18a2ec74bfd3793dc1aa0643fe55194575
Andrew <life.around.freaks@gmail.com>
2021-05-31 16:35:00 +0300

    Testing update contents.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from os import getenv as env
 from telegram import setup_bot
 from waitress import serve
+from json import loads
 
 server = Flask(__name__)
 handle_bot_update = setup_bot()
@@ -16,7 +17,13 @@ def ping():
 @server.route(f'/{env("TELEGRAM_BOT_TOKEN")}', methods=['POST'])
 def telegram_update():
 
-    handle_bot_update(request.stream.read().decode("utf-8"))
+    request_data = request.stream.read().decode("utf-8")
+
+    # TODO remove this part when done testing on live
+    print("-"*200)
+    print(loads(request_data))
+
+    handle_bot_update(request_data)
 
     return "!", 200
 
